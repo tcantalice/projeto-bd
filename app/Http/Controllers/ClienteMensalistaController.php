091b8e7999477ac5f3f6644cd03b3ee92a03a237
Brendosp <47647143+BrendoSouza@users.noreply.github.com>
2019-11-23 11:46:00 -0300

    Update ClienteMensalistaController.php
    Update Controller

--- app/Http/Controllers/ClienteMensalistaController.php
+++ app/Http/Controllers/ClienteMensalistaController.php
@@ -26,4 +26,42 @@ class ClienteMensalistaController extends Controller
 
         return redirect()->back()->with(['clientes' => $results]);
     }
+    
+    public function remove(Request $request, $matricula)
+    {
+        $clienteMensalista = ClienteMensalista::find($matricula);
+        
+        if($clienteMensalista){
+            try{
+                $flight->delete();
+            }catch{
+                return redirect()->back()->with([]);
+            }
+        }else{
+            return redirect()->back()-with([]);
+        }    
+    }
+    
+    public function create()
+    {
+        return "cliente_mensalista.index";
+    }
+    
+    public function store(Request $request)
+    {
+        $inputs = ['CLM_ID_MATRICULA' => $request->get('matricula'), 'CLM_DS_CPF' => $request->get('cpf'), 
+                   'CLM_DS_NOME' => $request->get('nome'), 'CLM_DT_NASCIMENTO' => $request->get('nascimento')];
+        
+        ClienteMensalista::insert($inputs);
+    }
+    
+    public function edit(Request $request)
+    {
+        return "cliente_mensalista.edit";
+    }
+    
+    public function update(Request $request, $matricula)
+    {
+        $clienteMensalista = ClienteMensalista::find($matricula);
+    }
 }
